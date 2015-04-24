e034540155927e00b9916efbdce636fa9c84b9dd
tyler-vs <tylervucf@knights.ucf.edu>
2015-04-24 02:07:47 -0400

    remove the article CRUD modules. only recipes module  exists now.

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var users = require('../../app/controllers/users.server.controller');
+var recipes = require('../../app/controllers/recipes.server.controller');
+
+
 module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
